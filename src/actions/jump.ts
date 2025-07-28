d86719c262b1dcf97e8f590ef9ae27ddaf3c57e0
Erickson Teixeira <rodox.k@hotmail.com>
2025-07-28 18:10:25 -0300

    stop animation while jumping

--- src/actions/jump.ts
+++ src/actions/jump.ts
@@ -1,20 +1,26 @@
 import Matter from 'matter-js';
+import { Dimensions } from 'react-native';
+
+const { height: HEIGHT } = Dimensions.get('window');
 
 const jump = (entities, { touches }) => {
-  let dino = entities.dino.body;
+  const dino = entities.dino.body;
+
+  const groundY = HEIGHT - 75;
 
   touches
     .filter((t) => t.type === 'press')
     .forEach(() => {
-      const velocityY = dino.velocity.y;
-
-      if (Math.abs(velocityY) < 0.01) {
+      if (Math.abs(dino.velocity.y) < 0.01) {
         Matter.Body.setVelocity(dino, {
           x: dino.velocity.x,
           y: -10,
         });
       }
     });
+
+  const isOnGround = dino.position.y >= groundY - 1;
+  entities.dino.isJumping = !isOnGround;
 
   return entities;
 };
--- src/components/Dino.tsx
+++ src/components/Dino.tsx
@@ -6,14 +6,14 @@ const images = [
   require('./../assets/images/dino_run2.png'),
 ];
 
-export default function Dino({ body, frame = 0, running }) {
-  console.log(frame, running)
+export default function Dino({ body, frame = 0, running, isJumping }) {
+  console.log(isJumping)
   const width = 40;
   const height = 50;
   const x = body.position.x - width / 2;
   const y = body.position.y - height / 6;
 
-  const imageSource = running ? images[frame] : require('./../assets/images/standing_still.png');
+  const imageSource = running && !isJumping ? images[frame] : require('./../assets/images/standing_still.png');
 
   return (
     <Image
